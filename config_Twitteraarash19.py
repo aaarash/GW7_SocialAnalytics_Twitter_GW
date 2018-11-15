4d6ebb1bf51c9098f511ba1590c676fed76769f3
aaarash <aarash@gwu.edu>
2018-11-14 22:03:39 -0500

    GW Social Analytics added

---
+++ config_Twitteraarash19.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "tyW6QC0GvqA5go76DIvhpPNH9"
+consumer_secret = "m3e3f7JZQojU1zkMVbxlpv36HGuOyj2Qon9FGrmpiCvWLWTrqp"
+access_token = "1016477356226211841-XH994ChAsTmcP9J1Dicd3q9AQevkdI"
+access_token_secret = "3RzMhQMEPGpNUBz2Y06NiCp3d2eQOiIcqsTbeQ7jqxpQI"
